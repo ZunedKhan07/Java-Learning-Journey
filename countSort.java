d44b3d3c7016ace2224738c33a9c33651e140aed
Zuned khan <zunedkhan107@gmail.com>
2025-09-20 16:38:53 +0530

    Implement CountSort algorithm with frequency and prefix sum logic

---
+++ countSort.java
@@ -0,0 +1,96 @@
+// public class countSort {
+
+//     public static void basicCountSort(int[] arr, int st, int end){
+//         // Find largest element in the arr
+//         int max = Integer.MIN_VALUE;
+//         for(int i=0; i<arr.length-1; i++){
+//             if (arr[i] > max) {
+//                 max =  arr[i];
+//             }
+//         }
+
+//         int[] count = new int[max+1];
+//         for(int i=0; i < arr.length; i++){
+//             count[arr[i]]++;
+//         }
+//         int k = 0;
+//         for(int i=0; i<count.length; i++){
+//             for(int j=0; j<count[i]; j++){
+//                 arr[k++] = i;
+//             }
+//         }
+//     }
+
+//     static void display(int[] arr){
+//         for (int i : arr) {
+//             System.out.println(i);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int arr[] = {4, 3, 5, 1, 9, 2};
+
+//         basicCountSort(arr, 0, arr.length);
+
+//         display(arr);
+//     }
+// }
+
+
+// isse stability nhi rehti and obj. nhi kr sakte isliye isko extend krni padti hai
+
+
+//                    🟢 Method - 2 🟢 
+
+
+public class countSort {
+
+    static int findMax(int[] arr){
+        // Find largest element in the arr
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length-1; i++){
+            if (arr[i] > max) {
+                max =  arr[i];
+            }
+        }
+        return max;
+    }
+
+    static void display(int[] arr){
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+
+    public static void countSort(int[] arr){
+        int n = arr.length;
+        int[] output = new int[n];
+        int max = findMax(arr);
+        int[] count = new int[max+1];
+        for(int i=0; i < n; i++){ // Make frequency array
+            count[arr[i]]++;
+        }
+        // Make prefix sum array of count array
+        for(int i=1; i < count.length; i++){
+            count[i] += count[i-1];
+        }
+
+        // Find the index of each elem in the original arr and put it in output arr
+        for(int i = n-1; i >= 0; i--){
+            int idx = count[arr[i]] - 1;
+            output[idx] = arr[i];
+            count[arr[i]]--;
+        }
+        // copy all elem of output to arr
+        for(int i=0; i<n; i++){
+            arr[i] = output[i];
+        }
+    }
+
+        public static void main(String[] args) {
+        int arr[] = {4, 3, 5, 1, 9, 2};
+
+        countSort(arr);
+
+        display(arr);
+    }
+}
